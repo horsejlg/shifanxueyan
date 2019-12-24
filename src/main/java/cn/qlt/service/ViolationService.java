fe647cbf0bf1b21026c95dd360bcc7709a5651a1
江立国 <horse_jlg@163.com>
2019-12-25 00:56:58 +0800

    查询语句添加条件

--- src/main/java/cn/qlt/service/ViolationService.java
+++ src/main/java/cn/qlt/service/ViolationService.java
@@ -20,21 +20,21 @@ public class ViolationService {
 	@Transactional
 	public PageResult findNotPass(Map<String, String> params, PageInfo page) {
 		PageResult result = new PageResult();
-		result.find(page, "from NotPass where 1=1 ", params, violationDao);
+		result.find(page, "from NotPass where 1=1 /~subject: subject.code = {subject} ~//~term: term.code={term} ~//~student: student.id={student}~/ ", params, violationDao);
 		return result;
 	}
 	
 	@Transactional
 	public PageResult findGeneralDiscipline(Map<String, String> params, PageInfo page) {
 		PageResult result = new PageResult();
-		result.find(page, "from GeneralDiscipline where 1=1 ", params, violationDao);
+		result.find(page, "from GeneralDiscipline where 1=1 /~term: term.code={term} ~//~student: student.id={student}~/", params, violationDao);
 		return result;
 	}
 	
 	@Transactional
 	public PageResult findSeriousDisciplinary(Map<String, String> params, PageInfo page) {
 		PageResult result = new PageResult();
-		result.find(page, "from SeriousDisciplinary where 1=1 ", params, violationDao);
+		result.find(page, "from SeriousDisciplinary where 1=1 /~term: term.code={term} ~//~student: student.id={student}~/", params, violationDao);
 		return result;
 	}
 	
